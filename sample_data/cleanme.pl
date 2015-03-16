347ed47d070470239c368e0463a6083bebff81e1
Brian Haas <brianjohnhaas@gmail.com>
2015-03-15 20:12:29 -0400

    add

---
+++ sample_data/cleanme.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+
+## we delete all files we don't need in this directory. Be careful in case users try running it somewhere else, outside this dir.
+chdir $FindBin::Bin or die "error, cannot cd to $FindBin::Bin";
+
+
+
+my @files_to_keep = qw (cleanme.pl 
+                        blat.spliced_alignments.gff3
+genome_sample.fasta
+gmap.spliced_alignments.gff3
+runMe.sh
+);
+
+
+my %keep = map { + $_ => 1 } @files_to_keep;
+
+
+foreach my $file (<*>) {
+	
+	if (-f $file && ! $keep{$file}) {
+		print STDERR "-removing file: $file\n";
+		unlink($file);
+	}
+}
+
+
+exit(0);
